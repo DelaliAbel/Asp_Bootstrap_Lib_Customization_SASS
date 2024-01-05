72bee81209f85e686bfd36c1c7fcf1b9aea60cc3
abel <abel>
2024-01-05 18:36:17 +0100

    Add project files.

---
+++ Asp_Bootstrap_Customization_SASS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Asp_Bootstrap_Customization_SASS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
